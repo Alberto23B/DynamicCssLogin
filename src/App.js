a4b596618482d2d018352a03fefdbeb2bab20e87
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2024-09-03 11:36:47 +0200

    define props and state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import logo from './assets/logo.svg';
 import './App.css';
 import LoginForm from "./LoginForm";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isValid, setIsValid] = useState(false);
+  const hasPageRenderedOnce = useRef(false);
+
+  useEffect(() => {
+    if (hasPageRenderedOnce.current) {
+      isValid ? alert("Correct") : alert("Incorrect")
+    }
+    hasPageRenderedOnce.current = true;
+    }, [isValid])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,7 +24,11 @@ function App() {
           Dynamic CSS Login Form
         </h1>
       </header>
-      <LoginForm />
+      <LoginForm 
+      username={username} setUsername={setUsername}
+      password={password} setPassword={setPassword}
+      setIsValid={setIsValid}
+      />
     </div>
   );
 }
--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-export default function LoginForm() {
+export default function LoginForm({username, setUsername, password, setPassword, setIsValid}) {
+
+    const testUsername = "testusername";
+    const testPassword = "mypassword"
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      if (username === testUsername && password === testPassword) {
+        setIsValid(true);
+      } else {
+        setIsValid(false);
+      }
+      setUsername("")
+      setPassword("");
+    }
+     
+    const handleReset = () => {
+        setUsername("")
+        setPassword("");
+    }
+
     return (
         <>
-          <form >
+          <form onSubmit={handleSubmit}>
             <label htmlFor="username">Username: </label>
-            <input id="username" name="username" type="text" placeholder="Insert Username" aria-label="username"/>
+            <input id="username" name="username"
+            value={username} onChange={(e) => setUsername(e.target.value)} 
+            type="text" placeholder="Insert Username" 
+            aria-label="username" required/>
             <label htmlFor="password">Password: </label>
-            <input id="password" name="password" type="text" placeholder="Insert Password" aria-label="password"/>
-            <input type="reset" value="Cancel" aria-label="cancel-button" />
+            <input id="password" name="password" 
+            value={password} onChange={(e) => setPassword(e.target.value)} 
+            type="text" placeholder="Insert Password" 
+            aria-label="password" required/>
+            <input type="reset" value="Cancel" onClick={handleReset} aria-label="cancel-button"/>
             <input type="submit" value="Submit" aria-label="submit-button" />
           </form>
         </>
